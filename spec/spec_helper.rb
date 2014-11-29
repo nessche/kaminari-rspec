3b91b982a30899ae43d63eee832ee500457e2bf4
Alan Larkin <alan.larkin@gmail.com>
2014-11-29 14:06:27 +0000

    Update to Kaminari ~> 0.16 and silence some deprecations.
    Also adds explict dependence upon rsepc ~> 3.
    
    Resolves #3.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,10 @@ require 'rspec'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  config.color_enabled = true
-  config.formatter     = 'documentation'
+  config.color     = true
+  config.formatter = 'documentation'
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
